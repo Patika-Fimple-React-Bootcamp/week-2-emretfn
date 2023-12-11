072230ba209a9db6abb817cee017adfc240771cd
Emre Tufan <emretufan42@gmail.com>
2023-12-11 14:49:52 +0300

    refactor: Refactor modal functionality and fix styling

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const loaderEl = document.querySelector(".loader");
 const mainEl = document.querySelector(".main");
 const searchInputEl = document.querySelector(".search-input");
+
+// Modal elements
+const modalEl = document.querySelector(".modal");
+const modalCloseEl = document.querySelector(".modal-close");
+const modalCommentsEl = document.querySelector(".modal-comment-list");
+const modalLoaderEl = document.querySelector(".modal-loader");
+const modalOverlayEl = document.querySelector(".modal-overlay");
+
 let data = [];
 
+// Fetch data
 const fetchData = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
   return data;
 };
 
+// Fetch comments for a post
+const fetchComments = async (id) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+  const data = await response.json();
+  return data;
+};
+
+// Render a card for each item in data
 const renderData = (data) => {
   //Reset main content
   mainEl.innerHTML = "";
@@ -41,9 +58,55 @@ const renderData = (data) => {
   deleteButtons.forEach((button) => {
     button.addEventListener("click", handleDelete);
   });
+
+  // Open a modal when a card is clicked
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    card.addEventListener("click", handleCardClick);
+  });
 };
 
+// Open modal and fetch comments
+const handleCardClick = async (e) => {
+  handleModalOpen();
+  const postId = e.currentTarget.id.split("-")[1];
+  const comments = await fetchComments(postId);
+
+  let innerHTML = "";
+  comments.forEach((comment) => {
+    innerHTML += `
+    <li>
+      <div class="modal-comment">
+        <div class="modal-comment-header">
+          <h3 class="modal-comment-name">${comment.name}</h3>
+          <span class="modal-comment-email">${comment.email}</span>
+        </div>
+        <p class="modal-comment-body">${comment.body}</p>
+      </div>
+    </li>`;
+  });
+  modalCommentsEl.innerHTML = innerHTML;
+  modalLoaderEl.style.display = "none";
+  modalCommentsEl.style.display = "flex";
+};
+
+// add and remove classes to show and hide modal
+const handleModalOpen = () => {
+  modalCommentsEl.innerHTML = "";
+  modalLoaderEl.style.display = "flex";
+  modalCommentsEl.style.display = "none";
+  modalEl.classList.add("show");
+  document.body.classList.add("no-scroll");
+};
+
+const handleModalClose = () => {
+  modalEl.classList.remove("show");
+  document.body.classList.remove("no-scroll");
+};
+
+// Delete a card
 const handleDelete = (e) => {
+  e.stopPropagation();
   // Confirm delete
   const isConfirmed = confirm("Are you sure you want to delete this post?");
   if (isConfirmed) {
@@ -55,6 +118,7 @@ const handleDelete = (e) => {
   }
 };
 
+// Filter data
 const handleFilter = (e) => {
   const searchTerm = e.target.value.toLowerCase();
   const filteredData = data.filter(
@@ -79,5 +143,7 @@ const init = async () => {
   data = await fetchData();
   renderData(data);
   searchInputEl.addEventListener("input", debounce(handleFilter, 500));
+  modalCloseEl.addEventListener("click", handleModalClose);
+  modalOverlayEl.addEventListener("click", handleModalClose);
 };
 init();
